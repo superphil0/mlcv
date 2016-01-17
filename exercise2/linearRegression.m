0dd9500c1b8c69e51860ce1be4d4079666820a14
Thomas Pinetz <thomaspinetz@gmail.com>
2016-01-17 10:28:57 +0100

    Added Linear Regression

---
+++ exercise2/linearRegression.m
@@ -0,0 +1,17 @@
+function w = linearRegression(x, y, w, gamma, N)
+    x_plot = [];
+    y_plot = [];
+    for i = 1:N
+        for idx = 1:size(x,1)
+            x_i = x(idx,:);
+            y_i = y(idx);
+            delta = -(y_i - w*x_i')* x_i;
+            w = w - gamma * delta;
+        end
+        x_plot(i) = i;
+        cost = 0.5 * sum((y - w*x') .^2);
+        y_plot(i) = cost;
+    end
+    
+    plot(x_plot,y_plot);
+end
